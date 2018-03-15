e65e1f432052e8d78ffdb3694548c2c1deff45e9
Jordi <jordi@shiraz.es>
2018-03-15 22:03:40 +0100

    Optimized model work

--- models_copy.py
+++ models_copy.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn import linear_model, metrics
 import matplotlib.pyplot as plt
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import mean_squared_error, r2_score, classification_report
 
 
 def read_data(filename):
@@ -20,96 +20,51 @@ def plot_model(y_test, predictions):
     plt.show()
 
 
-def model_columns():
+def logistic_regression_model_and_plot(filename_train, filename_test, x_columns_names, y_columns_names):
     # Training set
-    train = read_data('train_1_columns')
-
-    train.drop(train.columns[[0, 2, 3]], axis=1, inplace=True)
-    X_train = train.ix[:, 1:]
-
-    y_train = train["Alcoholic"]
-    y_train = pd.get_dummies(y_train)
-
-    print(X_train)
+    train_data = pd.read_csv(filepath_or_buffer=filename_train, sep=",", low_memory=False)
+    x_train = pd.get_dummies(train_data[x_columns_names])
+    y_train = pd.get_dummies(train_data[y_columns_names])
+    print(x_train)
     print(y_train)
 
     # Test set
-    test = read_data('test_1_columns')
-
-    test.drop(test.columns[[0, 2, 3]], axis=1, inplace=True)
-    X_test = test.ix[:, 1:]
-
-    y_test = test["Alcoholic"]
-    #y_test = pd.get_dummies(y_test)
+    test_data = pd.read_csv(filepath_or_buffer=filename_test, sep=",", low_memory=False)
+    x_test = pd.get_dummies(test_data[x_columns_names])
+    y_test = pd.get_dummies(test_data[y_columns_names])
+    print(x_test)
+    print(y_test)
 
     # Train model
-    # lr = linear_model.LinearRegression()
     lr = linear_model.LogisticRegression()
-    model = lr.fit(X_train, y_train)
+    model = lr.fit(x_train, y_train)
 
     # Make predictions using the testing set
-    y_pred = lr.predict(X_test)
+    y_pred = lr.predict(x_test)
 
     # The coefficients
-    print('Coefficients: \n', lr.coef_)
+    # print('Coefficients: \n', lr.coef_)
     # The mean squared error
-    #print("Mean squared error: %.2f" % mean_squared_error(y_test, y_pred))
+    # print("Mean squared error: %.2f" % mean_squared_error(y_test, y_pred))
     # Explained variance score: 1 is perfect prediction
-    #print('Variance score: %.2f' % r2_score(y_test, y_pred))
+    # print('Variance score: %.2f' % r2_score(y_test, y_pred))
 
-    #print(X_test['Min'].size)
-    print(y_test.size)
-
-    print('Model score:' + str(model.score(X_test, y_test)))
+    print('Model score:' + str(model.score(x_test, y_test)))
+    print(classification_report(y_test, y_pred))
 
     plot_model(y_test, y_pred)
 
 
-def model_rows():
-    # Training set
-    train = read_data('train_1_rows')
+#x_columns = ["Paradigm", "Replication", "Channel", 'Min', 'Max', 'Sd', 'Mean', 'Median', 'IQR', 'Q(0.025)', 'Q(0.25)', 'Q(0.75)', 'Q(0.975)']
+#y_columns = ["Alcoholic"]
 
-    #train.drop(train.columns[[0, 2, 4]], axis=1, inplace=True)
-    train.drop(train.columns[[0]], axis=1, inplace=True)
+#logistic_regression_model_and_plot("model_files/train_1_rows.csv", "model_files/test_1_rows.csv", x_columns, y_columns)
 
-    print(train)
 
-    X_train = train.ix[:, 1:]
-    X_train = pd.get_dummies(X_train)
-    y_train = train["Alcoholic"]
-    #y_train = pd.get_dummies(y_train)
+x_columns = ["Paradigm", "Channel"]
+for reading in range(256):
+    x_columns.append("Reading " + str(reading+1))
+y_columns = ["Alcoholic"]
 
-    print(X_train)
-    print(y_train)
+logistic_regression_model_and_plot("model_files/train_1_columns.csv", "model_files/test_1_columns.csv", x_columns, y_columns)
 
-    # Test set
-    test = read_data('test_1_rows')
-
-    test.drop(test.columns[[0]], axis=1, inplace=True)
-    X_test = test.ix[:, 1:]
-    X_test = pd.get_dummies(X_test)
-    y_test = test["Alcoholic"]
-
-    # Train model
-    lr = linear_model.LogisticRegression(C=1e5)
-    model = lr.fit(X_train, y_train)
-
-    # Make predictions using the testing set
-    y_pred = lr.predict(X_test)
-
-    # The coefficients
-    print('Coefficients: \n', lr.coef_)
-    # The mean squared error
-    #print("Mean squared error: %.2f" % mean_squared_error(y_test, y_pred))
-    # Explained variance score: 1 is perfect prediction
-    #print('Variance score: %.2f' % r2_score(y_test, y_pred))
-
-    #print(X_test['Min'].size)
-    print(y_test.size)
-
-    print('Model score:' + str(model.score(X_test, y_test)))
-
-    plot_model(y_test, y_pred)
-
-#model_columns()
-model_rows()
